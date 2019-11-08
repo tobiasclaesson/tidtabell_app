18969b0e7454241c711d260ab3229ecf002fe1ec
Tobias Claesson <tobbe.classon@hotmail.se>
2019-11-08 15:25:49 +0100

    fungerande version
    fixat script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //SL Platsuppslag API key: 5046296e5aff4afdb0c02d020ba4fa11
 //SL Realtidsinformation 4 API key: 34945e49ddbf432f8dbf2190c14eaf39
 
@@ -11,7 +12,7 @@ function loadVals(){
 
     currentStation = localStorage.getItem("_currentStation");
     walkTime = localStorage.getItem("_walkTime");
-    siteId = localStorage.getItem("_siteId");
+    //siteId = localStorage.getItem("_siteId");
 }
 function _loadVals(){
     //test för att se se variabler i consol.
@@ -27,14 +28,16 @@ function _loadVals(){
 function runFunction(){
     //Nollställer avgångar och kör funktionerna platsuppslag och realTimeInfo.
     document.getElementById("avgångar").innerHTML = "";
+    loadVals();
     platsuppslag(currentStation);
-    realTimeInfo(siteId);
+    //console.log(siteId + " runFunc");
+    //loadVals();
+    //realTimeInfo(siteId);
 }
 function platsuppslag(_currentStation){
     //Hämtar sideId för currentStation via Platsuppslag API
-    loadVals();
+    //loadVals();
     document.getElementById("avgångarFrån").innerHTML = "Avgångar från " + currentStation;
-    const div = document.getElementById("avgångar");
     const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/typeahead.json?key=5046296e5aff4afdb0c02d020ba4fa11&searchstring=" + currentStation + "&stationsonly=True&maxresults=1";
     fetch(url)
         .then((resp) => resp.json())
@@ -42,8 +45,10 @@ function platsuppslag(_currentStation){
         let infos = data.ResponseData;
         
             return infos.map(function (info) {
-                localStorage.setItem("_siteId", info.SiteId);
-                
+                siteId = info.SiteId;
+                //localStorage.setItem("_siteId", info.SiteId);
+                console.log(siteId + " platsupp");
+                realTimeInfo(siteId);
             })
         })
     .catch(function (error) {
@@ -52,8 +57,17 @@ function platsuppslag(_currentStation){
 }
 function realTimeInfo(_siteId){
     //Skriver ut avgångsinformation med data från Realtidsinformation API.
-    loadVals();
+    //loadVals();
+    //let currentTime = Date.parse("2019-11-08T10:55:25");
+    //let currentTime = Date.parse("YYYY-MM-DDTHH:mm:ss");
+    console.log(_siteId + " realtimeinfo1");
+    let currentTime = new Date();
+    let avgångTime;
     let text = "";
+    let diffMins;
+    let diffWalk;
+    let gåOm;
+    
     const div = document.getElementById("avgångar");
     const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.json?key=34945e49ddbf432f8dbf2190c14eaf39&siteid=" + _siteId + "&timewindow=25";
     fetch(url)
@@ -62,11 +76,44 @@ function realTimeInfo(_siteId){
             let infos = data.ResponseData.Metros;
 
             return infos.map(function (info) {
-                if (info.DisplayTime != "Nu" && info.DisplayTime >= walkTime){
-                    
-                    text = info.LineNumber + " " + info.Destination + " " + info.DisplayTime + "<br>";
-                    div.innerHTML += text;
-                } 
+                avgångTime = new Date(Date.parse(info.ExpectedDateTime));
+                
+
+                diffMins = (avgångTime - currentTime) / 1000 / 60;
+                diffWalk = diffMins - walkTime;
+                gåOm = (Math.floor(diffMins - walkTime)) + " min";
+                if (gåOm == (0 + " min")) {
+                    gåOm = "Nu";
+                }
+                if(diffWalk > 0){
+                    //Körs om "Nu" finns med i DisplayTime Stringen.
+                    if (info.DisplayTime == "Nu"){
+                        info.DisplayTime = "0 min"
+                        
+                        text = info.LineNumber + " " + info.Destination + " " + info.DisplayTime + " " + gåOm + "<br>";
+                        div.innerHTML += text;
+                    }
+                    //Körs om tecknet ":" finns i DisplayTime.
+                    else if (info.DisplayTime.indexOf(":") > -1) {
+                        avgångTime = new Date(Date.parse(info.ExpectedDateTime));
+                        
+                        diffMins = (avgångTime - currentTime) / 1000 / 60;
+
+                        info.DisplayTime = Math.floor(diffMins) + " min";
+
+                        text = info.LineNumber + " " + info.Destination + " " + info.DisplayTime + " " + gåOm + "<br>";
+                        div.innerHTML += text;
+                    }
+
+                    else{
+                        //Körs om inga andra villkor stämmer.
+                        text = info.LineNumber + " " + info.Destination + " " + info.DisplayTime + " " + gåOm + "<br>";
+                        div.innerHTML += text;
+                    }
+                }
+                /* else{
+                    div.innerHTML += "Fel";
+                } */
             })
         })
     .catch(function (error) {
@@ -82,7 +129,7 @@ function laddaData(){
         let infos = data.data;
         return infos.map(function (info) {
             document.getElementById("stationTextBox").value = info.Station;
-            document.getElementById("walkTimeBox").value = info.WalkTime
+            document.getElementById("walkTimeBox").value = info.WalkTime;
 
             
         })
@@ -133,3 +180,6 @@ function goBack(){
     document.location.href = "index.html";
 }
 
+/* diff = info.ExpectedDateTime - currentTime;
+                        diffMins = Math.round(diff / 60000);
+                        console.log(diffMins); */
