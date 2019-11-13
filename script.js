b0ab42c6f88412d13b6dae7c5b5110a73fe19f36
Tobias Claesson <tobbe.classon@hotmail.se>
2019-11-13 12:39:47 +0100

    script/style update

--- script.js
+++ script.js
@@ -56,51 +56,86 @@ function realTimeInfo(_siteId){
     let diffMins;
     let diffWalk;
     let gåOm;
-    
+    let i = 1;
+
     const div = document.getElementById("avgångTable");
-    const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.json?key=34945e49ddbf432f8dbf2190c14eaf39&siteid=" + _siteId + "&timewindow=25";
+    const url = "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/realtimedeparturesV4.json?key=34945e49ddbf432f8dbf2190c14eaf39&siteid=" + _siteId + "&timewindow=120";
     fetch(url)
             .then((resp) => resp.json())
             .then(function (data) {
             let infos = data.ResponseData.Metros;
 
             return infos.map(function (info) {
-                //Räknar ut mellanskillnaden medan tid till avgång och walkTime.
-                avgångTime = new Date(Date.parse(info.ExpectedDateTime));
-                diffMins = (avgångTime - currentTime) / 1000 / 60;
-                diffWalk = diffMins - walkTime;
-                gåOm = (Math.floor(diffMins - walkTime)) + " min";
-                //Sätter variabeln gåOm till "Nu" om avgången går om 0 min.
-                if (gåOm == (0 + " min")) {
-                    gåOm = "Nu";
+                //kortar ner Destinationsträngen till max 11 bokstäver + ".." för att man ska
+                //se att strängen är nedkortad.
+                let dest = info.Destination;
+                if (dest.length > 11) {
+                    dest = dest.substring(0,11) + "..";
                 }
-                
-                if(diffWalk > 0){
-                    //Körs om "Nu" finns med i DisplayTime Stringen.
-                    if (info.DisplayTime == "Nu"){
-                        info.DisplayTime = "0 min"
-                        
-                        text = "<tr><td>" + info.LineNumber + " " + info.Destination + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm;
-                        div.innerHTML += text;
-                    }
-                    //Körs om tecknet ":" finns i DisplayTime.
-                    else if (info.DisplayTime.indexOf(":") > -1) {
-                        avgångTime = new Date(Date.parse(info.ExpectedDateTime));
-                        
-                        diffMins = (avgångTime - currentTime) / 1000 / 60;
-
-                        info.DisplayTime = Math.floor(diffMins) + " min";
-
-                        text = "<table><tr><td id='destCol'>" + info.LineNumber + " " + info.Destination + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm + "</td></tr></table>";
-                        div.innerHTML += text;
-                    }
-
-                    else{
-                        //Körs om inga andra villkor stämmer.
-                        text = "<table><tr><td id='destCol'>" + info.LineNumber + " " + info.Destination + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm + "</td></tr></table>";
-                        div.innerHTML += text;
+                //Gör så att endast de första 15 avgångarna visas
+                if (i <= 15){
+                    //Räknar ut mellanskillnaden medan tid till avgång och walkTime.
+                    avgångTime = new Date(Date.parse(info.ExpectedDateTime));
+                    diffMins = (avgångTime - currentTime) / 1000 / 60;
+                    diffWalk = diffMins - walkTime;
+                    gåOm = (Math.floor(diffMins - walkTime)) + " min";
+                    //Sätter variabeln gåOm till "Nu" om avgången går om 0 min.
+                    if (gåOm == (0 + " min")){
+                        gåOm = "Nu";
                     }
                     
+                    if(diffWalk > 0){
+                        //Körs om "Nu" finns med i DisplayTime Stringen.
+                        if (info.DisplayTime == "Nu"){
+                            info.DisplayTime = "0 min"
+                            
+                            //Om variabeln gåOm är mindre än 5 skrivs den ut i röd text, annars skrivs allt ut i gult.
+                            if (diffMins < 6) {
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td id='gåOmCol'>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                            else{
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                        }
+                        //Körs om tecknet ":" finns i DisplayTime.
+                        else if (info.DisplayTime.indexOf(":") > -1){
+                            avgångTime = new Date(Date.parse(info.ExpectedDateTime));
+                            
+                            diffMins = (avgångTime - currentTime) / 1000 / 60;
+
+                            info.DisplayTime = Math.floor(diffMins) + "min";
+                            //Om variabeln gåOm är mindre än 5 skrivs den ut i röd text, annars skrivs allt ut i gult.
+                            if (diffMins < 6) {
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td id='gåOmCol'>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                            else{
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                        }
+                        else{
+                            //Körs om inga andra villkor stämmer.
+                            
+                            //Om variabeln gåOm är mindre än 5 skrivs den ut i röd text, annars skrivs allt ut i gult.
+                            if (diffMins < 6) {
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td id='gåOmCol'>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                            else{
+                                text = "<table><tr><td id='lineCol'>" + info.LineNumber + "</td><td id='destCol'>" + dest + "</td><td>" + info.DisplayTime + "</td><td>" + gåOm + "</td></tr></table>";
+                                div.innerHTML += text;
+                                i++;
+                            }
+                        }
+                    }
                 }
             })
         })
